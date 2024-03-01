8817134007786df71c773a4d6049220a6b4c3cf8
adelewatkins <adele_watkins@hotmail.com>
2024-03-01 15:54:28 +0000

    got items to render on the screen

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -11,6 +11,7 @@ function CartItems() {
     function shoppingBasket() {
         axios.get("http://localhost:8082/cart/get/" + params.id)
             .then((response) => {
+                console.log("response" + response.data); 
                 setShopping(response.data);
             })
             .catch((err) => console.error(err))
@@ -34,10 +35,22 @@ function CartItems() {
             <br />
             <br />
             <h1>Items in your cart</h1>
-            
+            <ul>
+                {shopping.items && shopping.items.length > 0 ? (
+                    shopping.items.map(item => (
+                        <li key={item.id}>
+                            <div>Name: {item.name}</div>
+                            <div>Price: {"£" + item.price}</div>
+                            <div>Quantity: {item.quantity}</div>
+                        </li>
+                    ))
+                ) : (
+                    <p>No items in the cart</p>
+                )}
+            </ul>
+           
         </div>
-    )
-
+    );
 }
 
 export default CartItems;
