17c5600e22ceb1f74dcffa874c7f9695ee8bbbb9
adelewatkins <adele_watkins@hotmail.com>
2024-03-01 16:25:29 +0000

    item totals added

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { Card } from 'react-bootstrap';
 
 function CartItems() {
     const [shopping, setShopping] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
     const navigate = useNavigate();
     const params = useParams();
 
@@ -13,12 +16,31 @@ function CartItems() {
             .then((response) => {
                 console.log("response" + response.data); 
                 setShopping(response.data);
+                calculateTotalItems(response.data.items);
+                calculateCartTotal(response.data.items);
             })
             .catch((err) => console.error(err))
     }
 
     useEffect(shoppingBasket, [])
     console.log(shopping)
+
+    function calculateTotalItems(items) {
+        let total = 0;
+        items.forEach(item => {
+            total += 1;
+        });
+        setTotalItems(total);
+    
+    }
+
+    function calculateCartTotal(items) {
+        let total = 0;
+        items.forEach(item => {
+            total += item.price
+        });
+        setCartTotal(total);
+    }
 
 
 
@@ -35,20 +57,27 @@ function CartItems() {
             <br />
             <br />
             <h1>Items in your cart</h1>
-            <ul>
-                {shopping.items && shopping.items.length > 0 ? (
-                    shopping.items.map(item => (
-                        <li key={item.id}>
-                            <div>Name: {item.name}</div>
-                            <div>Price: {"£" + item.price}</div>
-                            <div>Quantity: {item.quantity}</div>
-                        </li>
-                    ))
-                ) : (
-                    <p>No items in the cart</p>
-                )}
-            </ul>
-           
+            <h3>Number of items: {totalItems}</h3>
+            <h3>Cart Total: £{cartTotal}</h3>
+            <div className="row">
+            {shopping.items && shopping.items.length > 0 ? (
+                shopping.items.map(item => (
+                    <div key={item.id} className="col-md-4">
+                        <Card style={{ width: '18rem', marginBottom: '20px' }}>
+                            <Card.Body>
+                                <Card.Title>{item.name}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{"£" + item.price}</Card.Subtitle>
+                                <Card.Text>
+                                    Quantity: {item.quantity}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))
+            ) : (
+                <p>No items in the cart</p>
+            )}
+        </div>
         </div>
     );
 }
