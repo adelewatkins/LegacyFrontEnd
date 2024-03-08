2a26105805a66e92628333fff05e622515ea727f
2023temitope <topbeemak@yahoo.com>
2024-03-08 09:53:49 +0000

    loginupdate

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,36 +7,45 @@ import { useNavigate } from 'react-router-dom';
 import e from 'cors';
 
 
-const Login = ()=> {
-    const navigate=useNavigate();
-    const handleSubmit= (e)=> {e.preventDefault();
-    navigate("/home")}
-    
+const Login = () => {
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/Home")
+    }
 
-    
-return(
-    <div className='wrapper'>
-        <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <div className = "input-box">
-            <input type ="text" placeholder='Username' required /><FaRegUser className='icon' />
-    </div>
-    <div className = "input-box">
-            <input type ="password" placeholder='Password' required /><FaLock className='icon'/>
-    </div>
-    <div className = "remember-forgot">
-         <label><input type ="checkbox" />Remember me </label>
 
-    </div>
-    <button onClick={() =>
-                navigate("/home")}type ="submit">Login</button>
-    <div className= "register-link">
-    <a href='https://http.dog/403.jpg'>Forgot password?</a>
-        <p>Don't have an account?<a href="#"></a></p>
+
+    return (
+
+        <div className='wrapper'>
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+
+                <div className="input-box">
+                    <input type="text" placeholder='Username' required /><FaRegUser className='icon' />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required /><FaLock className='icon' />
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me </label>
+                    <a href='https://http.dog/403.jpg'>Forgot password?</a>
+                </div>
+                {/* <button onClick={() => */}
+                <button type="submit">Login</button>
+                {/* navigate("/home")}type ="submit">Login</button> */}
+
+                <div className="register-link">
+                    <p>Don't have an account?  <a href="#"></a></p>
+
+                    <div className="input-box">
+                        <input type="register" placeholder='Register' />
+                    </div>
+                </div>
+            </form>
         </div>
-        </form>
-        </div>
-);
+    );
 
 
 }
